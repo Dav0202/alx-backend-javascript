02ef82305bc1e6089e3ed720e122aa9ec44e477f
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-03-28 13:58:02 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,13 @@
+function getStudentIdsSum(students) {
+  if (!Array.isArray(students)) {
+    return [];
+  }
 
+  const reducer = (arr, item) => arr + item.id;
+
+  const sumIds = students.reduce(reducer, 0);
+
+  return sumIds;
+}
+
+export default getStudentIdsSum;
