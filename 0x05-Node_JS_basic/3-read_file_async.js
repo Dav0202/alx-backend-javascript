e3b8a75417ba9b8fa05153419bcae5e91b9d4420
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-15 21:29:15 +0100

    Update 3-read_file_async.js
    upload

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,47 @@
+const fs = require('fs');
 
+function countStudents(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+      const response = [];
+      let msg;
+
+      const req = data.toString().split('\n');
+
+      let get_student = req.filter((item) => item);
+
+      get_student = get_student.map((item) => item.split(','));
+
+      const NUMBER_OF_STUDENTS = get_student.length ? get_student.length - 1 : 0;
+      msg = `Number of students: ${NUMBER_OF_STUDENTS}`;
+
+      response.push(msg);
+
+      const data_dict = {};
+      for (const i in get_student) {
+        if (i !== 0) {
+          if (!data_dict[get_student[i][3]]) data_dict[get_student[i][3]] = [];
+
+          data_dict[get_student[i][3]].push(get_student[i][0]);
+        }
+      }
+
+      delete data_dict.field;
+
+      for (const key of Object.keys(data_dict)) {
+        msg = `Number of students in ${key}: ${
+          data_dict[key].length
+        }. List: ${data_dict[key].join(', ')}`;
+
+        response.push(msg);
+      }
+      resolve(response);
+    });
+  });
+}
+
+module.exports = countStudents;
