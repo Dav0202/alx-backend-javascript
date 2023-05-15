9327c1e0a50a43c08bfb7107838eaf1313664ef5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-15 21:17:50 +0100

    Update 2-read_file.js
    Update and upload function

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -1,1 +1,41 @@
+const fs = require('fs');
 
+function countStudents(path) {
+  let req;
+
+  try {
+    req = fs.readFileSync(path);
+  } catch (err) {
+    throw new Error('Cannot load the database');
+  }
+
+  req = req.toString().split('\n');
+
+  let get_student = req.filter((item) => item);
+
+  get_student = get_student.map((item) => item.split(','));
+
+  const NUMBER_OF_STUDENTS = get_student.length ? get_student.length - 1 : 0;
+  console.log(`Number of get_student: ${NUMBER_OF_STUDENTS}`);
+
+  const data_dict = {};
+  for (const i in get_student) {
+    if (i !== 0) {
+      if (!data_dict[get_student[i][3]]) data_dict[get_student[i][3]] = [];
+
+      data_dict[get_student[i][3]].push(get_student[i][0]);
+    }
+  }
+
+  delete data_dict.field;
+
+  for (const key of Object.keys(data_dict)) {
+    console.log(
+      `Number of get_student in ${key}: ${data_dict[key].length}. List: ${data_dict[
+        key
+      ].join(', ')}`,
+    );
+  }
+}
+
+module.exports = countStudents;
