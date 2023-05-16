7f204c059f1ad8d1a22da4381cfd48de474599b0
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-16 14:01:37 +0100

    Update server.js

--- 0x05-Node_JS_basic/full_server/server.js
+++ 0x05-Node_JS_basic/full_server/server.js
@@ -1,1 +1,12 @@
+import express from 'express';
+import controllerRoute from './routes/index';
 
+const app = express();
+const port = 1245;
+
+controllerRoute(app);
+
+app.listen(port, () => {
+});
+
+export default app;
