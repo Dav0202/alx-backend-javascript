0fa763aa48f0fc57016a635a671ad5cec3b28d45
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-16 16:29:20 +0100

    Update 2-read_file.js

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -16,22 +16,24 @@ function countStudents(path) {
   get_student = get_student.map((item) => item.split(','));
 
   const NUMBER_OF_STUDENTS = get_student.length ? get_student.length - 1 : 0;
-  console.log(`Number of student: ${NUMBER_OF_STUDENTS}`);
+  console.log(`Number of students: ${NUMBER_OF_STUDENTS}`);
 
-  const data_dict = {};
+  const fields = {};
   for (const i in get_student) {
     if (i !== 0) {
-      if (!data_dict[get_student[i][3]]) data_dict[get_student[i][3]] = [];
+      if (!fields[get_student[i][3]]) fields[get_student[i][3]] = [];
 
-      data_dict[get_student[i][3]].push(get_student[i][0]);
+      fields[get_student[i][3]].push(get_student[i][0]);
     }
   }
 
-  delete data_dict.field;
+  delete fields.field;
 
-  for (const key of Object.keys(data_dict)) {
+  for (const key of Object.keys(fields)) {
     console.log(
-      `Number of students in ${key}: ${data_dict[key].length}. List: ${data_dict[key].join(', ')}`,
+      `Number of students in ${key}: ${fields[key].length}. List: ${fields[
+        key
+      ].join(', ')}`,
     );
   }
 }
