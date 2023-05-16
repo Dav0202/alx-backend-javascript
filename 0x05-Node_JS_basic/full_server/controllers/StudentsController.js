47694aa9e1107b50fa2ab4b1c1fc143ab996f662
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-16 13:20:43 +0100

    Update StudentsController.js

--- 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
+++ 0x05-Node_JS_basic/full_server/controllers/StudentsController.js
@@ -1,1 +1,25 @@
+import readDatabase from '../utils';
 
+class StudentsController {
+  static getAllStudents(request, response, DATABASE) {
+    readDatabase(DATABASE)
+      .then((fields) => {
+        const get_student = [];
+
+        let msg;
+
+        get_student.push('This is the list of our students');
+
+        for (const key of Object.keys(fields)) {
+          msg = `Number of students in ${key}: ${
+            fields[key].length
+          }. List: ${fields[key].join(', ')}`;
+
+          get_student.push(msg);
+        }
+        response.send(200, `${get_student.join('\n')}`);
+      })
+      .catch(() => {
+        response.send(500, 'Cannot load the database');
+      });
+  }
