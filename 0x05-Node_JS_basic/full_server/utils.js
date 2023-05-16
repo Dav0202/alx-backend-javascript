db2105c5e9b0c929901c895626b9fd28771ba3a5
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-16 12:49:31 +0100

    Update utils.js

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,1 +1,33 @@
+const fs = require('fs');
 
+function readDatabase(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error(err));
+        return;
+      }
+      const get_data = data.toString().split('\n');
+
+      let get_student = get_data.filter((item) => item);
+
+      get_student = get_student.map((item) => item.split(','));
+
+      const fields = {};
+      for (const i in get_student) {
+        if (i !== 0) {
+          if (!fields[get_student[i][3]]) fields[get_student[i][3]] = [];
+
+          fields[get_student[i][3]].push(get_student[i][0]);
+        }
+      }
+
+      delete fields.field;
+
+      resolve(fields);
+
+    });
+  });
+}
+
+export default readDatabase;
