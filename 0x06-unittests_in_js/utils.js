765f46406d0a338c4d90220e63aa02f79891f4d2
Dav0202 <96493230+Dav0202@users.noreply.github.com>
2023-05-18 10:44:42 +0100

    Update utils.js

--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,20 @@
+const Utils = {
+  calculateNumber(type, a, b) {
+  const a_data = Math.round(a);
+  const b_data = Math.round(b);
 
+  if (type === 'SUBTRACT') {
+    return a_data - b_data;
+  }
+
+  if (type === 'DIVIDE') {
+    if (b_data === 0) {
+      return 'Error';
+    }
+    return a_data / b_data;
+  }
+
+  return a_data + b_data;
+  }
+}
+module.exports = Utils;
